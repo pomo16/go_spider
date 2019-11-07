2364bca7e8a9215583cac2f05bc3a82b20b3f80b
pomo16 <m13143378602@163.com>
2019-11-07 12:36:44 +0800

    add url

--- consts/consts.go
+++ consts/consts.go
@@ -7,3 +7,13 @@ var appMap = map[string]string{
 func GetAppMap() map[string]string {
 	return appMap
 }
+
+//爬虫URL
+const (
+	COMMENT_URL_PREFIX = "https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?cc=cn&displayable-kind=11&sort=0&appVersion=all"
+	VERSION_URL_PREFIX = "https://itunes.apple.com/rss/customerreviews/"
+	VERSION_URL_SUFFIX = "/sortby=mostrecent/json?l=en&&cc=cn"
+)
+
+//时间格式化模板
+const TIME_STR = "2006-01-02 15:04:05"
