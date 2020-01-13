57c4dc144c88aa1fcf704b2a8d244986d278478b
pomo16 <m13143378602@163.com>
2020-01-13 14:05:50 +0800

    fix script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 RUN_NAME="go_spider"
 
 mkdir output output/${RUN_NAME}_log
+cp -r config output/
 export GO111MODULE=on
 go build -a -o output/${RUN_NAME}_dev
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o output/${RUN_NAME}
